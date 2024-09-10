3465ce1f2fbad7dcdddd82bf789b2a213f7b4ba9
jadabrunson <148473515+jadabrunson@users.noreply.github.com>
2024-09-10 19:54:31 -0400

    Update Color_RepeatApp.swift

--- Color Repeat/Color_RepeatApp.swift
+++ Color Repeat/Color_RepeatApp.swift
@@ -6,22 +6,23 @@
 //
 
 import SwiftUI
-import SwiftData
 import FirebaseCore
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()  // Ensure Firebase is configured
+        return true
+    }
 }
+
 @main
 struct Color_RepeatApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .modelContainer(for: Item.self) // Setup SwiftData for Item model
         }
     }
 }
